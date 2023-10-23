3a3653e35c3df5b59db920502e8349acdd102c18
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2023-10-23 17:53:21 -0300

    Added sample accounts, ran methods, set up acocunt number generation

--- BankAccount.py
+++ BankAccount.py
@@ -1,32 +1,78 @@
+# importing 'random' to allow for random account number generation
 import random
 
+# creating a BankAccount class
 class BankAccount:
-    def __init__(self):
-        self.full_name = 'full name'
-        self.account_number = random.randint(10000000, 99999999)
+    def __init__(self, full_name, account_number = None):
+        self.full_name = full_name
+        if account_number is None:
+            self.account_number = random.randint(10000000, 99999999)
+        else: 
+            self.account_number = account_number 
         self.balance = 0.0
-    
+
+    # Account number generator 
+    def account_number_generator(self):
+        self.account_number = random.randint(10000000, 99999999)
+
+    # Desposit method
     def deposit(self, amount):
         self.balance += amount
         print(f'Amount Deposited: ${amount}. New Balance: ${self.balance}')
-
+    # Withdraw method
     def withdraw(self, amount):
         self.balance -= amount
         if self.balance < 0:
             self.balance -= 10
             print(f'Insufficient funds. You have been charged an overdraft fee of $10.')
         print(f'Amount Withdrawn: ${amount}. New Balance: ${self.balance}')
-
+    # Get balance method
     def get_balance(self):
         print(f'Your balance is ${self.balance}')
         return self.balance
-    
+    # Add interest method
     def add_interest(self):
         interest = self.balance * 0.00083
         self.balance += interest
-    
+    # Print statement method
     def print_statement(self):
         print(self.full_name)
         print(f'Account No.: {self.account_number}')
         print(f'Balance: ${self.balance}')
+
+# creating a new instance of the BankAccount class, running methods
+# 1/3
+rileys_account = BankAccount('Riley Nowak')
+rileys_account.deposit(100)
+rileys_account.withdraw(50)
+rileys_account.add_interest()
+rileys_account.get_balance()
+rileys_account.print_statement()
+
+# creating a new instance of the BankAccount class, running methods
+# 2/3
+timbers_account = BankAccount('Timber')
+timbers_account.deposit(500)
+timbers_account.withdraw(550)
+timbers_account.add_interest()
+timbers_account.get_balance()
+timbers_account.print_statement()
+
+# creating a new instance of the BankAccount class, running methods
+# 3/3
+snickers_account = BankAccount('Snickers')
+snickers_account.deposit(10000)
+snickers_account.withdraw(20)
+snickers_account.add_interest()
+snickers_account.get_balance()
+snickers_account.print_statement()
+
+# creating Mitchell's account and running methods
+mitchells_account = BankAccount('Mitchell Hudson', '03141592')
+mitchells_account.deposit(400000)
+mitchells_account.print_statement()
+mitchells_account.add_interest()
+mitchells_account.print_statement()
+mitchells_account.withdraw(150)
+mitchells_account.print_statement()
 
--- README.md
+++ README.md
@@ -6,11 +6,11 @@
     - [x] full_name - the full name of the bank account account owner.
     - [x] account_number - randomly generated 8 digit number, unique per account.
     - [x] balance - the balance of money in the account, should start at 0.
-- [ ] Then define the following methods for the BankAccount class:
+- [x] Then define the following methods for the BankAccount class:
     - [x] deposit method
         - [x] will take one parameter amount and will add amount to the balance.
         - [x] will print the message: “Amount deposited: $X.XX new balance: $Y.YY”
-    - [ ] withdraw method 
+    - [x] withdraw method 
         - [x] will take one parameter amount and will subtract amount from the balance. 
         - [x] will print a message, like “Amount withdrawn: $X.XX new balance: $Y.YY”.
         - [x] If the user tries to withdraw an amount that is greater than the current balance, print ”Insufficient funds.” and charge them with an overdraft fee of $10
@@ -27,16 +27,16 @@
             Account No.: ****5678
             Balance: $100.00
             ```
-- [ ] Outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
-    - [ ] Your examples should show you using the different methods above to demonstrate them working.
-- [ ] Include example code to do the following:
-    - [ ] Create a new bank account instance: user: "Mitchell", account number: 03141592.
-    - [ ] Deposit $400,000 into "Mitchell's" account.
-    - [ ] Print a statement
-    - [ ] Add interest to the account
-    - [ ] Print a statement
-    - [ ] Make a withdrawl of $150 (Mitchell needs some Yeezy's)
-    - [ ] Print a statement
+- [x] Outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
+    - [x] Your examples should show you using the different methods above to demonstrate them working.
+- [x] Include example code to do the following:
+    - [x] Create a new bank account instance: user: "Mitchell", account number: 03141592.
+    - [x] Deposit $400,000 into "Mitchell's" account.
+    - [x] Print a statement
+    - [x] Add interest to the account
+    - [x] Print a statement
+    - [x] Make a withdrawl of $150 (Mitchell needs some Yeezy's)
+    - [x] Print a statement
 
 ### Stretch Challenges (Optional)
 - [ ] Add an attribute to set the account type: checking or savings.
